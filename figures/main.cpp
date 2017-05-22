b2d0e6a5d4834dbdaff018d9c0a978e31e13f0a5
Anna <anchen.oktober@gmail.com>
2017-05-22 22:16:16 +0300

    final fix

--- figures/main.cpp
+++ figures/main.cpp
@@ -10,7 +10,7 @@ class figure{
         }
         void print() {  std::cout << "Figures name: " << name << "\n"; };
 
-        ~figure(){
+        virtual ~figure(){
             std::cout << "figure is destroyed\n";
         };
 };
@@ -48,25 +48,26 @@ class circle : public figure {
 int main() {
 
     int size = 100;
-    figure arr[size];
+    figure **arr = new figure*[size];
+
     for (int i = 0; i < size; i++){
         int random_num = random()%3;
         if (random_num == 0){
-            triangle *t = new triangle;
-            arr[i] = *t;
+            figure * t = new triangle();
+            arr[i] = t;
         }
         else if(random_num == 1) {
-            square *s = new square;
-            arr[i] = *s;
+            figure * s = new square();
+            arr[i] = s;
         }
         else {
-            circle *c = new circle;
-            arr[i] = *c;
+            figure * c = new circle();
+            arr[i] = c;
         }
     }
     for (int i = 0; i < size; i++){
-        arr[i].print();
-        //delete arr[i];
+        arr[i]->print();
+        delete arr[i];
     }
 
     return 0;
